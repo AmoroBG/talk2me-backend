26639eb1f1734d22d382acd88d2980d7638b87d4
Amoro <amorobg@gmail.com>
2022-03-12 20:28:28 +0000

    Setup project structure

---
+++ app.js
@@ -0,0 +1,30 @@
+// REQUIRE PACKAGES
+const express = require('express')
+const dotenv = require('dotenv')
+// INTERNAL MODULES
+const dbconnect = require('./server/database/dbconnection')
+const userRoutes=require("./routes/user")
+
+// LOAD CONFIG
+dotenv.config({ path: './server/config/config.env' })
+
+// APP
+const app = express()
+
+// DB CONNECTION
+dbconnect
+
+// MIDDLEWARE
+// body-parser
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+// ROUTES
+app.use("/users", userRoutes)
+
+
+// SERVER
+const PORT = process.env.PORT || 3000
+app.listen(PORT, function () {
+  console.log(`Server started on port ${PORT}`)
+})
