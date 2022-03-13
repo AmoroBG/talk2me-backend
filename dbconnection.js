2d8768e80c7cd96dac6ce746860cc63a84ce4bf7
Amoro <amorobg@gmail.com>
2022-03-13 09:05:03 +0000

    Modify config and dbconnection

---
+++ dbconnection.js
@@ -0,0 +1,23 @@
+const path = require('path')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+
+// LOAD CONFIG
+dotenv.config({
+  path: path.resolve(__dirname, '../config/config.env'),
+})
+
+// Connecting to Database
+const dbconnect = mongoose
+  .connect(process.env.DATABASE_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(function () {
+    console.log('Successfully connected to db')
+  })
+  .catch(function (err) {
+    console.log(err)
+  })
+
+module.exports = dbconnect
